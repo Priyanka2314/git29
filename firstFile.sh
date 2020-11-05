279fca4d5a16de3e6b29480113fb82908fc44d10
Priyanka <priyankacs102@gmail.com>
2020-11-05 20:05:26 +0530

    adding sum to firstfile

--- firstFile.sh
+++ firstFile.sh
@@ -1,2 +1,8 @@
 echo "This is the firstFile"
 echo "This is first intialisation"
+echo "Enter numbers you want to calculate"
+read -p "Enter number1=>" x
+read -p "Enter number2=>" y
+((sum=x+y))
+echo $sum
+
